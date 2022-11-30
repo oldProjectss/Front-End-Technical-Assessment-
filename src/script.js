c478d9002ae35c2b13e476e75adc577b3702232f
Ismail Courr <contact@ismailcourr.ml>
2022-12-01 00:57:49 +0100

    Use const instead of let

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const canva = document.getElementById('app');
 // This function creates the input elements
 const createArea = () => {
   // Create the input element
-  let newArea = document.createElement('input');
+  const newArea = document.createElement('input');
   // Add a class to the input to style it and make it easier to select
   newArea.classList.add('input');
   // Add the type of input (text) and the placeholder
@@ -15,9 +15,10 @@ const createArea = () => {
   // Make sure the next input is in focus and ready to be typed in
   newArea.focus();
 
-  // Add an event listener to the input for lintning to '/h1/ command to make sure the text is converted to an h1 style
+  // Add an event listener to the input for listening for the '/h1/ command
+  // to make sure the text is converted to an h1 style
   newArea.addEventListener('input', (e) => {
-    let value = e.target.value;
+    const value = e.target.value;
     if (value === '/h1') {
       e.target.value = '';
       e.target.classList.add('h1');
@@ -28,7 +29,8 @@ const createArea = () => {
   // or the 'Backspace' key to delete the current input
   newArea.addEventListener('keydown', (e) => {
     // If there is no text in the input and the user presses the 'Backspace' key, delete the input
-    if (e.target.value === '' && e.key === 'Backspace') {
+    if (e.target.value === ''
+        && e.key === 'Backspace') {
       //  Make sure to delet the 'h1' class when the 'Backspace' key is pressed  if no text is present
       // and remove the input if the area if it's empty and styling is removed
       if (e.target.classList.contains('h1')) {
